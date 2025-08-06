a0fb57542fb08e41045729e0d0efc1c7370aecb3
aayanaqdas <aayan.aqdas@outlook.com>
2025-08-06 23:36:38 +0200

    added nanoid to give unique ids to each Die component. rearranged the setDice state to take an object instead of single numbers. added a hold function that changes the isHeld state in the object to get a green color to the dice you clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
-import {useState} from "react"
+import { useState } from "react";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
   const [dice, setDice] = useState(genAllNewDice());
 
   function genAllNewDice() {
-    const randomNumber = () => Math.ceil(Math.random() * 6);
-    const numArray = Array.from({ length: 10 }, randomNumber);
-    return numArray;
+    const numArray = new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return numArray.map((num) => ({ value: num, isHeld: false, id: nanoid() }));
   }
 
-  const diceEl = dice.map(num => {
-    return <Die value={num} />
-  })
-  
+  const diceEl = dice.map((dieObj) => {
+    return (
+      <Die
+        key={dieObj.id}
+        value={dieObj.value}
+        id={dieObj.id}
+        isHeld={dieObj.isHeld}
+        onClick={hold}
+      />
+    );
+  });
+
+  function hold(id) {
+    setDice((prev) =>
+      prev.map((item) => {
+        return item.id === id ? { ...item, isHeld: !item.isHeld } : item;
+      })
+    );
+  }
+
   return (
     <main>
-      <div className="die-container">
-        {diceEl}
-      </div>
-      <button className="roll-btn" onClick={() => setDice(genAllNewDice())}>Roll</button>
+      <div className="die-container">{diceEl}</div>
+      <button className="roll-btn" onClick={() => setDice(genAllNewDice())}>
+        Roll
+      </button>
     </main>
   );
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,5 +1,11 @@
-export default function Die(props){
-    return(
-        <button className="die-component">{props.value}</button>
-    )
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#fff",
+  };
+
+  return (
+    <button className="die-component" style={styles} onClick={() => props.onClick(props.id)}>
+      {props.value}
+    </button>
+  );
 }
